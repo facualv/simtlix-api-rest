3df636a703d872ff347dff1466f34c28a070a667
facualv <turcoalv@gmail.com>
2020-06-06 12:52:47 -0300

    [ADD] some comments added

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,15 @@ const { AuthorRoutes, BookRoutes } = require("./index.routes");
 
 module.exports = function () {
   const router = express.Router();
+
   //Default Middlewares
   router.use(express.json()).use(morgan("dev"));
+
+  //Routes
   router.use("/books", BookRoutes());
   router.use("/authors", AuthorRoutes());
+
+  //Other Middlewares
   router.use(NotFoundMiddleware);
   router.use(ErrorMiddleware);
   return router;
